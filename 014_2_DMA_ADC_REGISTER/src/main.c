31bafe1f8ca8b19960648299a137764e45760b29
Muhammed Fatih KÖSEOGLU <49070304+mfatihkoseoglu@users.noreply.github.com>
2020-08-26 01:30:34 +0300

    Update main.c
    c

--- 014_2_DMA_ADC_REGISTER/src/main.c
+++ 014_2_DMA_ADC_REGISTER/src/main.c
@@ -10,14 +10,13 @@ void RCC_Config()
 	while(!(RCC->CR & 0x00020000));	// HSEON Ready Flag wait
 	RCC->CR |= 0x00080000;	// CSS Enable
 	RCC->CR |= 0x01000000;	// PLL ON
-	RCC->PLLCFGR |= 0x00400000;	// PLL e HSE seçtik
+	RCC->PLLCFGR |= 0x00400000;	// PLL e HSE seÃ§tik
 	RCC->PLLCFGR |= 0x00000004;	// PLL M = 4
 	RCC->PLLCFGR |= 0x00005A00;	// Pll N = 168
 	RCC->PLLCFGR |= 0x00000000;	// PLL p = 2
 	RCC->CFGR |= 0x00000000;	// AHB Prescaler = 1
 	RCC->CFGR |= 0x00080000;	// APB2 Prescaler = 2
 	RCC->CFGR |= 0x00001400;	// APB1 Prescaler = 4
-	RCC->CIR |= 0x00080000;		// HSERDY Flag clear
 	RCC->CIR |= 0x00800000;		// CSS Flag clear
 }
 
