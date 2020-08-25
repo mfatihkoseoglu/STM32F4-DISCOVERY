46b5ab9e1ea1e7fe141a8e9f0a4658c0c6d833ba
Muhammed Fatih KÖSEOGLU <49070304+mfatihkoseoglu@users.noreply.github.com>
2020-08-26 01:26:47 +0300

    Update main.c
    clk

--- 001_SYSTEM_CLK_CONFIG_WITH_REGISTER/src/main.c
+++ 001_SYSTEM_CLK_CONFIG_WITH_REGISTER/src/main.c
@@ -6,27 +6,15 @@ void CLK_Config()
 	RCC->CR |= 0x00010000;	// HSEON enable
 	while(!(RCC->CR & 0x00020000));	// HSEON Ready Flag wait
 	RCC->CR |= 0x00080000;	// CSS Enable
-	RCC->PLLCFGR |= 0x00400000;	// PLL e HSE seçtik
+	RCC->PLLCFGR |= 0x00400000;	// PLL e HSE seÃ§tik
 	RCC->PLLCFGR |= 0x00000004;	// PLL M = 4
 	RCC->PLLCFGR |= 0x00005A00;	// Pll N = 168
 	RCC->PLLCFGR |= 0x00000000;	// PLL p = 2
 	RCC->CFGR |= 0x00000000;	// AHB Prescaler = 1
 	RCC->CFGR |= 0x00080000;	// APB2 Prescaler = 2
 	RCC->CFGR |= 0x00001400;	// APB1 Prescaler = 4
-	RCC->CIR |= 0x00080000;		// HSERDY Flag clear
 	RCC->CIR |= 0x00800000;		// CSS Flag clear
 
-	RCC->AHB1ENR = 0x00000008;
-
-	GPIOD->MODER = 0x55000000;
-	GPIOD->OTYPER = 0x00000000;
-	GPIOD->OSPEEDR = 0xFF000000;
-	GPIOD->PUPDR = 0x00000000;
-}
-
-void delay(uint32_t time)
-{
-	while(time--);
 }
 
 int main(void)
@@ -34,10 +22,7 @@ int main(void)
 	CLK_Config();
   while (1)
   {
-	  GPIOD->ODR = 0x0000F000;
-	  delay(16800000);
-	  GPIOD->ODR = 0x00000000;
-	  delay(16800000);
+	  // SystemClock 168 MHz
   }
 }
 
