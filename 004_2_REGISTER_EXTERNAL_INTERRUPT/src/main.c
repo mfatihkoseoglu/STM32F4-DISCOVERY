864d7247aa902fa8430164350ae99d9b3450b765
Muhammed Fatih KÖSEOGLU <49070304+mfatihkoseoglu@users.noreply.github.com>
2020-08-26 01:17:46 +0300

    Update main.c
    commit

--- 004_2_REGISTER_EXTERNAL_INTERRUPT/src/main.c
+++ 004_2_REGISTER_EXTERNAL_INTERRUPT/src/main.c
@@ -11,14 +11,13 @@ void CLK_Config()
 	RCC->CR |= 0x00030000;	// HSEON and HSEONRDY enable
 	while(!(RCC->CR & 0x00020000));	// HSEON Ready Flag wait
 	RCC->CR |= 0x00080000;	// CSS Enable
-	RCC->PLLCFGR |= 0x00400000;	// PLL e HSE seçtik
+	RCC->PLLCFGR |= 0x00400000;	// PLL e HSE seÃ§tik
 	RCC->PLLCFGR |= 0x00000004;	// PLL M = 4
 	RCC->PLLCFGR |= 0x00005A00;	// Pll N = 168
 	RCC->PLLCFGR |= 0x00000000;	// PLL p = 2
 	RCC->CFGR |= 0x00000000;	// AHB Prescaler = 1
 	RCC->CFGR |= 0x00080000;	// APB2 Prescaler = 2
 	RCC->CFGR |= 0x00001400;	// APB1 Prescaler = 4
-	RCC->CIR |= 0x00080000;		// HSERDY Flag clear
 	RCC->CIR |= 0x00800000;		// CSS Flag clear
 }
 
@@ -34,7 +33,7 @@ void GPIO_Config()
 
 void EXTI_Config()
 {
-	RCC->APB2ENR = 0x00004000;	// SYSCFG active (extý)
+	RCC->APB2ENR = 0x00004000;	// SYSCFG active (extÃ½)
 
 	SYSCFG->EXTICR[0] = 0x00000000;
 
@@ -42,9 +41,9 @@ void EXTI_Config()
 	NVIC_EnableIRQ(EXTI1_IRQn);
 	NVIC_EnableIRQ(EXTI2_IRQn);
 
-	NVIC_SetPriority(EXTI0_IRQn, 0);
-	NVIC_SetPriority(EXTI1_IRQn, 1);
-	NVIC_SetPriority(EXTI2_IRQn, 2);
+	NVIC_SetPriority(EXTI0_IRQn, 0); // externel interrupt0 priority 0 (very high)
+	NVIC_SetPriority(EXTI1_IRQn, 1); // external interrupt1 priority 1 (high)
+	NVIC_SetPriority(EXTI2_IRQn, 2); // external interrupt2 priority 2 (low)
 
 	EXTI->IMR = 0x00000007;
 	EXTI->RTSR = 0x00000007;
@@ -52,22 +51,19 @@ void EXTI_Config()
 
 void EXTI0_IRQHandler()
 {
-	if(EXTI->PR & 0x00000001)
+	if(EXTI->PR & 0x00000001)   // interrupt flag active 
 	{
-		GPIOD->ODR = 0x00001000;
-		delay(33600000);
-
+	    GPIOD->ODR = 0x00001000; // gpiod pin 12 set other reset
 	}
-	EXTI->PR = 0x00000001;
+	EXTI->PR = 0x00000001;      // interrupt flag reset 
+
 }
 
 void EXTI1_IRQHandler()
 {
 	if(EXTI->PR & 0x00000002)
 	{
-		GPIOD->ODR = 0x00002000;
-		delay(33600000);
-
+	    GPIOD->ODR = 0x00002000; // GPIOD pin 13 set other reset 
 	}
 	EXTI->PR = 0x00000002;
 }
@@ -76,21 +72,20 @@ void EXTI2_IRQHandler()
 {
 	if(EXTI->PR & 0x00000004)
 	{
-		GPIOD->ODR = 0x00004000;
-		delay(33600000);
-
+	    GPIOD->ODR = 0x00004000; // GPIOD pin 14 set other reset
 	}
 	EXTI->PR = 0x00000004;
 }
 
 int main(void)
 {
-	CLK_Config();
-	GPIO_Config();
-	EXTI_Config();
+    CLK_Config();
+    GPIO_Config();
+    EXTI_Config();
+    GPIOD->ODR = 0x00000000;   // reset all pins
   while (1)
   {
-	  GPIOD->ODR = 0x0000F000;
+     
   }
 }
 
